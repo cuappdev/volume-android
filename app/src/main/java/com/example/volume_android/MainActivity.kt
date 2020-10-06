9f502560268e7ba1949740014b6eeced2f0d6d41
as3484 <as3484@cornell.edu>
2020-10-06 13:07:59 -0400

    Changing from Java to Kotlin

---
+++ app/src/main/java/com/example/volume_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.volume_android
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
