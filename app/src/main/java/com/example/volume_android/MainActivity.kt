3fa5e771c426ce4a66750f2662e8ae6f42867f0a
Justin <jiangj2620@gmail.com>
2020-10-07 17:57:22 -0400

    Create basic webview with scroll listener

--- app/src/main/java/com/example/volume_android/MainActivity.kt
+++ app/src/main/java/com/example/volume_android/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.volume_android
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.example.volume_android.views.ObservableWebView
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView: ObservableWebView = findViewById(R.id.webview)
+        webView.loadUrl("https://www.nyt.com/")
+
+        //Code to prevent from launching in external browser
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+        }
     }
 }
