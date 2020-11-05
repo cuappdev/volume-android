dff0883c0b67b029270d00c9fcdeda0f70e362cc
Justin <jiangj2620@gmail.com>
2020-11-04 19:04:27 -0500

    add listener

--- app/src/main/java/com/example/volume_android/views/ObservableWebView.kt
+++ app/src/main/java/com/example/volume_android/views/ObservableWebView.kt
@@ -3,7 +3,10 @@ package com.example.volume_android.views
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import android.webkit.WebView
+import com.example.volume_android.R
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class ObservableWebView @JvmOverloads constructor(
         context: Context,
@@ -11,11 +14,26 @@ class ObservableWebView @JvmOverloads constructor(
         defStyleAttr: Int = 0
 ) : WebView(context, attrs, defStyleAttr) {
 
+
+    private lateinit var top: WebviewTop
+    private lateinit var bot: WebviewBottom
+
+    fun setTopBot(top1: WebviewTop, bot1: WebviewBottom){
+        top = top1
+        bot = bot1
+    }
+
     override fun onScrollChanged(scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int) {
         super.onScrollChanged(scrollX, scrollY, oldScrollX, oldScrollY)
         when {
-            scrollY > oldScrollY -> {Log.d("WebView", "scrollDown")}
-            scrollY < oldScrollY -> {Log.d("WebView", "scrollUp")}
+            scrollY > oldScrollY -> {
+                top.minimize(true)
+                bot.minimize(true)
+            }
+            scrollY < oldScrollY -> {
+                top.minimize(false)
+                bot.minimize(false)
+            }
         }
         Log.d("WebView", "Scrolled")
     }
