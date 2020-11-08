6cf76e46fb9b6b5a7b4b24264278bda3ad0aacca
as3484 <as3484@cornell.edu>
2020-11-08 11:56:38 -0500

    tabbed layout working, but icons are missing'

---
+++ app/src/main/java/com/example/volume_android/PagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.volume_android
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.volume_android.fragments.HomeFragment
+import com.example.volume_android.fragments.PublicationsFragment
+import com.example.volume_android.fragments.SavedPublicationsFragment
+
+private const val COUNT = 3
+
+class PagerAdapter(fragmentManager: FragmentManager): FragmentPagerAdapter(fragmentManager) {
+
+    override fun getCount(): Int {
+        return COUNT
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> HomeFragment()
+            1 -> PublicationsFragment()
+            2 -> SavedPublicationsFragment()
+            else -> return HomeFragment()
+        }
+    }
+
+    
+}
