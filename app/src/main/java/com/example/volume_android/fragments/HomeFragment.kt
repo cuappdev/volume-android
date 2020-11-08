79760bb85d7bbb1ae47f5c5bc55251786ddec0ef
as3484 <as3484@cornell.edu>
2020-11-08 11:56:38 -0500

    tabbed layout working, but icons are missing'

---
+++ app/src/main/java/com/example/volume_android/fragments/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.volume_android.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.volume_android.R
+
+class HomeFragment: Fragment() {
+
+    companion object{
+        fun newInstance(): HomeFragment = HomeFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+}
