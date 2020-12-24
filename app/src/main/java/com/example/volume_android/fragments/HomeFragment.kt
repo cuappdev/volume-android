2375a53c031112b992e6aeb16755a165ec0d79dd
Justin <jiangj2620@gmail.com>
2020-12-24 01:05:58 -0600

    rxjava and trending articles

--- app/src/main/java/com/example/volume_android/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/volume_android/fragments/HomeFragment.kt
@@ -1,22 +1,23 @@
 package com.example.volume_android.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.volume_android.OnboardingFragHolder
 import com.example.volume_android.R
 import com.example.volume_android.adapters.BigReadHomeAdapter
 import com.example.volume_android.adapters.HomeFollowingArticleAdapters
 import com.example.volume_android.models.Article
-import com.example.volume_android.models.Publication
 import com.example.volume_android.networking.GraphQlUtil
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
 
 class HomeFragment(val articles: List<Article>) : Fragment() {
 
@@ -24,16 +25,37 @@ class HomeFragment(val articles: List<Article>) : Fragment() {
     private lateinit var bigRedRv: RecyclerView
     private lateinit var followingRv: RecyclerView
     private lateinit var otherArticles: RecyclerView
+    private lateinit var disposables: CompositeDisposable
+
+
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view1 = inflater.inflate(R.layout.home_fragment, container, false)
-        bigRedRv = view1.findViewById(R.id.big_red_rv)!!
-        bigRedRv.adapter = BigReadHomeAdapter(articles)
-        val linearLayoutManager : LinearLayoutManager = LinearLayoutManager(view1.context)
-        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
-        bigRedRv.layoutManager = linearLayoutManager
+
+        disposables = CompositeDisposable()
+
+
+        val graphQlUtil = GraphQlUtil()
+
+        //Get Trending Articles
+        val trendingObs = graphQlUtil.getTrendingArticles(10.0, "2020-12-10T12:34:20.000Z").subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+        disposables.add(trendingObs.subscribe{
+            var trendingArticles = mutableListOf<Article>()
+
+            it.data?.getTrendingArticles?.mapTo(trendingArticles, { it -> Article(it.articleURL, it.date.toString(), it.id, it.imageURL, it.publicationID, it.shoutouts, it.title)
+            })
+
+            bigRedRv = view1.findViewById(R.id.big_red_rv)!!
+
+            bigRedRv.adapter = BigReadHomeAdapter(trendingArticles)
+            val linearLayoutManager : LinearLayoutManager = LinearLayoutManager(view1.context)
+            linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+            bigRedRv.layoutManager = linearLayoutManager
+        })
+
+
 
         followingRv = view1.findViewById(R.id.follwing_rv)
         val linearLayoutManager2: LinearLayoutManager = LinearLayoutManager(view1.context)
