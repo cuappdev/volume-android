8e72f881b1feada9b8f2c6b26f6f19e454ce282b
as3484 <as3484@cornell.edu>
2020-12-28 17:15:51 -0500

    Made changes to models to reflect changes made to backend models

--- app/src/main/java/com/example/volume_android/models/Publication.kt
+++ app/src/main/java/com/example/volume_android/models/Publication.kt
@@ -1,10 +1,23 @@
 package com.example.volume_android.models
 
-class Publication ( val bio: String,
-                    val id: String,
-                    val imageURL: String,
-                    val name: String,
-                    val rssName: String,
-                    val rssURL: String,
-                    val shoutouts: Double,
-                    val websiteURL: String) {}
+class Publication (
+//        val bio: String,
+//                    val id: String,
+//                    val imageURL: String,
+//                    val name: String,
+//                    val rssName: String,
+//                    val rssURL: String,
+//                    val shoutouts: Double,
+//                    val websiteURL: String
+    val id: String,
+    val backgroundImageURL: String,
+    val bio: String,
+    val name: String,
+    val profileImageURL: String,
+    val rssName: String,
+    val rssURL: String,
+    val slug: String,
+    val shoutouts: Float,
+    val websiteURL: String,
+    val mostRecentArticle: Article
+) {}
