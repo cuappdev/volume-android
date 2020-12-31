0e33ab3708d26eaf249162ba63a9ce6c501ead3e
as3484 <as3484@cornell.edu>
2020-12-30 22:34:30 -0500

    Added networking for more publications

--- app/src/main/java/com/example/volume_android/fragments/PublicationsFragment.kt
+++ app/src/main/java/com/example/volume_android/fragments/PublicationsFragment.kt
@@ -10,13 +10,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.volume_android.R
 import com.example.volume_android.adapters.FollowPublicationsAdapter
 import com.example.volume_android.adapters.FollowingHorizontalAdapter
+import com.example.volume_android.models.Article
 import com.example.volume_android.models.Publication
+import com.example.volume_android.util.GraphQlUtil
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class PublicationsFragment(val publications: List<Publication>) : Fragment() {
 
     private lateinit var followpublicationRV : RecyclerView
     private lateinit var morepublicationRV: RecyclerView
-
+    val graphQlUtil = GraphQlUtil()
+    val disposables = CompositeDisposable()
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -31,12 +37,28 @@ class PublicationsFragment(val publications: List<Publication>) : Fragment() {
         followpublicationRV.setHasFixedSize(true)
 
 
-        morepublicationRV = view.findViewById(R.id.follwing_more_publications_rv)
-        morepublicationRV.adapter = FollowPublicationsAdapter(publications, view.context)
-        morepublicationRV.layoutManager = LinearLayoutManager(view.context)
-        morepublicationRV.setHasFixedSize(true)
+        getMorePublications(view)
 
 
         return view
+    }
+
+    fun getMorePublications(view: View){
+        val otherObs = graphQlUtil.getAllPublications().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+        disposables.add(otherObs.subscribe{
+            var others = mutableListOf<Publication>()
+
+            it.data?.getAllPublications?.mapTo(others, { it -> Publication(id = it.id, backgroundImageURL = it.backgroundImageURL, bio = it.bio, name = it.name, profileImageURL = it.profileImageURL, rssName = it.rssName, rssURL = it.rssURL, slug = it.slug, shoutouts = it.shoutouts, websiteURL = it.websiteURL) })
+            //otherArticles = view1.findViewById(R.id.other_articlesrv)
+            //val linearLayoutManager3: LinearLayoutManager = LinearLayoutManager(view1.context)
+            //otherArticles.layoutManager = linearLayoutManager3
+            //otherArticles.adapter = HomeFollowingArticleAdapters(others)
+
+            morepublicationRV = view.findViewById(R.id.follwing_more_publications_rv)
+            morepublicationRV.adapter = FollowPublicationsAdapter(others, view.context)
+            morepublicationRV.layoutManager = LinearLayoutManager(view.context)
+            morepublicationRV.setHasFixedSize(true)
+
+        })
     }
 }
