c9eaf049d1b4ed4c1b7be1ae227218cf7e8402ff
ckdesir <christopher.desir01@gmail.com>
2021-03-16 16:50:10 -0400

    Reduce repetition in PublicationsFragment.kt by introducing isRefreshing

--- app/src/main/java/com/cornellappdev/volume/adapters/BigReadHomeAdapter.kt
+++ app/src/main/java/com/cornellappdev/volume/adapters/BigReadHomeAdapter.kt
@@ -1,9 +1,7 @@
 package com.cornellappdev.volume.adapters
 
 import android.content.Intent
-import android.graphics.BlurMaskFilter
 import android.os.Build
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,9 +15,6 @@ import com.cornellappdev.volume.R
 import com.cornellappdev.volume.models.Article
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.vertical_article_home_card.view.*
-import java.time.Duration
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 
 class BigReadHomeAdapter(private val articles: MutableList<Article>) :
@@ -27,15 +22,17 @@ class BigReadHomeAdapter(private val articles: MutableList<Article>) :
 
     class BigReadArticleVH(itemView: View) : RecyclerView.ViewHolder(itemView){
         val pubName: TextView = itemView.vert_card_pub_name
-        val articleTitle : TextView = itemView.article_title_big_read
-        val articleImg : ImageView = itemView.vert_img_view
+        val articleTitle: TextView = itemView.article_title_big_read
+        val articleImg: ImageView = itemView.vert_img_view
         val postTime: TextView = itemView.big_read_card_layout_time
         val shoutoutCount: TextView = itemView.big_read_card_layout_shoutouts
         val layout: ConstraintLayout = itemView.big_read_layout
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BigReadArticleVH {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.vertical_article_home_card, parent, false)
+        val itemView = LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.vertical_article_home_card, parent, false)
         return BigReadArticleVH(itemView)
     }
 
@@ -48,7 +45,7 @@ class BigReadHomeAdapter(private val articles: MutableList<Article>) :
         val currentItem : Article = articles[position]
         holder.articleTitle.text = currentItem.title
         Article.applyNSFWFilter(currentItem, holder.articleTitle)
-        if(!currentItem.imageURL.isNullOrEmpty()){
+        if (!currentItem.imageURL.isNullOrEmpty()) {
             Picasso.get().load(currentItem.imageURL).into(holder.articleImg)
         } else if (!currentItem.publication?.profileImageURL.isNullOrEmpty()) {
             Picasso.get()
@@ -60,8 +57,7 @@ class BigReadHomeAdapter(private val articles: MutableList<Article>) :
         Article.setCorrectDateText(currentItem, holder.postTime)
         holder.shoutoutCount.text = currentItem.shoutouts?.toInt().toString() + " shout-outs"
         holder.pubName.text = currentItem.publication!!.name
-
-        holder.layout.setOnClickListener{
+        holder.layout.setOnClickListener {
             val intent = Intent(holder.layout.context, MainActivity::class.java)
             intent.putExtra("article", currentItem)
             holder.layout.context?.startActivity(intent)
--- app/src/main/java/com/cornellappdev/volume/adapters/FollowingHorizontalAdapter.kt
+++ app/src/main/java/com/cornellappdev/volume/adapters/FollowingHorizontalAdapter.kt
@@ -18,19 +18,16 @@ class FollowingHorizontalAdapter(private val followedPublications: MutableList<P
         RecyclerView.Adapter<FollowingHorizontalAdapter.FollowHorizontalVH>() {
 
     class FollowHorizontalVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val pub_logo : ImageView = itemView.vertical_publication_logo
-        val pub_name : TextView = itemView.vertical_publication_name
+        val pub_logo: ImageView = itemView.vertical_publication_logo
+        val pub_name: TextView = itemView.vertical_publication_name
         val layout: ConstraintLayout = itemView.vert_cert_layout
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowHorizontalVH {
-        val itemView =
-                LayoutInflater.from(parent.context).inflate(
-                        R.layout.vertical_circular_publication_card,
-                        parent,
-                        false)
+        val itemView = LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.vertical_circular_publication_card, parent, false)
         return FollowHorizontalVH(itemView)
-
     }
 
     override fun getItemCount(): Int {
@@ -38,14 +35,11 @@ class FollowingHorizontalAdapter(private val followedPublications: MutableList<P
     }
 
     override fun onBindViewHolder(holder: FollowHorizontalVH, position: Int) {
-        val currentItem : Publication = followedPublications[position]
-
-        if(!currentItem.profileImageURL.isNullOrBlank()) {
+        val currentItem = followedPublications[position]
+        if (!currentItem.profileImageURL.isNullOrBlank()) {
             Picasso.get().load(currentItem.profileImageURL).into(holder.pub_logo)
         }
-
         holder.pub_name.text = currentItem.name
-
         holder.layout.setOnClickListener {
             val intent = Intent(holder.layout.context, PublicationProfileActivity::class.java)
             intent.putExtra("publication", currentItem)
