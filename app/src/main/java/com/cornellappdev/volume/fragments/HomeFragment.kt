4f81ca1dd7cd1ec8914ba4c56010c9275ce802d2
ckdesir <christopher.desir01@gmail.com>
2021-10-27 16:35:06 -0400

    Add refresh cover indicator to HomeFragment.kt

--- app/src/main/java/com/cornellappdev/volume/fragments/HomeFragment.kt
+++ app/src/main/java/com/cornellappdev/volume/fragments/HomeFragment.kt
@@ -77,6 +77,7 @@ class HomeFragment : Fragment() {
             // Re-populates the RecyclerViews on refresh, is dependent on whether or not they are
             // initialized.
             setOnRefreshListener {
+                binding.cover.visibility = View.VISIBLE
                 setUpHomeView(
                         binding, isRefreshing = (
                         this@HomeFragment::bigRedRV.isInitialized &&
@@ -133,6 +134,7 @@ class HomeFragment : Fragment() {
                         dialog,
                         NoInternetDialog.TAG
                 ).commit()
+                binding.cover.visibility = View.GONE
             } else {
                 childFragmentManager.findFragmentByTag(NoInternetDialog.TAG).let { dialogFrag ->
                     (dialogFrag as? DialogFragment)?.dismiss()
@@ -467,6 +469,7 @@ class HomeFragment : Fragment() {
                 // shimmer off
                 binding.shimmerOtherArticles.visibility = View.GONE
                 otherRV.visibility = View.VISIBLE
+                binding.cover.visibility = View.GONE
             })
         }
     }
