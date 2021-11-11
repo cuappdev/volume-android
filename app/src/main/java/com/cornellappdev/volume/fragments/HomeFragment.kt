632edb9ba7af96420403529e045654a3e6876267
ckdesir <christopher.desir01@gmail.com>
2021-11-11 14:23:17 -0500

    Optimize imports in HomeFragment.kt

--- app/src/main/java/com/cornellappdev/volume/fragments/HomeFragment.kt
+++ app/src/main/java/com/cornellappdev/volume/fragments/HomeFragment.kt
@@ -1,12 +1,9 @@
 package com.cornellappdev.volume.fragments
 
-import MyDiffCallback
-import android.app.Activity
+import com.cornellappdev.volume.MyDiffCallback
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
