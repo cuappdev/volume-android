cea409cf6ccd13dde1f5b8ca6a02af8c06b2f192
ckdesir <christopher.desir01@gmail.com>
2021-11-17 18:11:38 -0500

    Add parsing of Notification data to OnboardingActivity.kt

--- app/src/main/java/com/cornellappdev/volume/util/ActivityForResultConstants.kt
+++ app/src/main/java/com/cornellappdev/volume/util/ActivityForResultConstants.kt
@@ -2,5 +2,6 @@ package com.cornellappdev.volume.util
 
 enum class ActivityForResultConstants(val code: Int) {
     FROM_NO_INTERNET(1),
-    FROM_PUBLICATION_PROFILE_ACTIVITY(2)
+    FROM_PUBLICATION_PROFILE_ACTIVITY(2),
+    FROM_MAIN_ACTIVITY(3)
 }
